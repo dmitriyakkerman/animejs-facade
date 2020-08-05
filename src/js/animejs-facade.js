584e913cfa24280fcf513fe69540fab0698f3e92
dmitriy-akkerman <dmitriy.akkerman.odesseo@gmail.com>
2020-08-05 17:53:30 +0300

    initial commit

---
+++ src/js/animejs-facade.js
@@ -0,0 +1,21 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  } else {
+    root.AnimeFacade = factory();
+  }
+}(typeof self !== 'undefined' ? self : this, function () {
+
+  class AnimeFacade {
+    constructor() {
+
+    }
+  }
+
+  window.AnimeFacade = AnimeFacade;
+
+  return AnimeFacade;
+
+}));

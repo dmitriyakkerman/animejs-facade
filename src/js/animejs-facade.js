54b4efe1957e13d990b10dec427bea6391ee5b51
dmitriy-akkerman <dmitriy.akkerman.odesseo@gmail.com>
2020-08-06 14:52:50 +0300

    initial commit

--- src/js/animejs-facade.js
+++ src/js/animejs-facade.js
@@ -9,8 +9,66 @@
 }(typeof self !== 'undefined' ? self : this, function () {
 
   class AnimeFacade {
-    constructor() {
+    constructor(targets, options) {
 
+      if(!targets) {
+        throw new Error('No target selector')
+      }
+
+      this.targets = targets;
+      this.options = options || {};
+      this.getTargetOnScroll();
+      this.getPresets();
+      this.getPresetValue();
+    }
+
+    getTargetOnScroll() {
+
+      let that = this;
+
+      window.addEventListener('scroll', function() {
+        that.targets.forEach(function(targetElement) {
+          let windowHeight = window.innerHeight;
+          let targetPosition = targetElement.getBoundingClientRect().top;
+          if (targetPosition - windowHeight <= 0) {
+            that.initAnime(targetElement, that.getPresetValue());
+          }
+        })
+      })
+    }
+
+    getPresets() {
+
+      let presets = {
+        translateX: '100%'
+      }
+
+      return presets;
+    }
+
+    getPresetValue() {
+
+      let that = this;
+
+      let matchedObject;
+
+        for(let preset in that.getPresets()) {
+          if(that.getPresets().hasOwnProperty(preset)) {
+           matchedObject = JSON.stringify({ [preset]: that.getPresets()[preset] });
+          }
+      }
+      return matchedObject;
+    }
+
+    initAnime(targetElement, preset) {
+
+      let that = this;
+
+      let a = Object.assign({
+        targets: targetElement
+      }, JSON.parse(preset));
+
+      anime(a);
     }
   }
 

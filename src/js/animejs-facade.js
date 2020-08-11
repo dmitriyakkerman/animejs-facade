c59015bec09435e2c996fbc2619a13438aff99b2
dmitriy-akkerman <dmitriy.akkerman.odesseo@gmail.com>
2020-08-11 15:50:10 +0300

    1. входной элемент - строка или нода 2. инициилизация по "DOMContentLoaded", запуск по "Scroll" 3. возможность задавать начальные и конечные знаничения для элемента

--- src/js/animejs-facade.js
+++ src/js/animejs-facade.js
@@ -9,7 +9,25 @@
 }(typeof self !== 'undefined' ? self : this, function () {
 
   let presets = {
-    translateX: '100%'
+    translateX: '100%',
+    translateY: '100%',
+    scale: 1,
+    rotate: '0',
+    width: '100%',
+    height: '100%',
+    opacity: 1,
+    backgroundColor: '#FFF',
+    borderRadius: ['0%', '50%'],
+  }
+
+  let defaults = {
+    preset: {
+      name: 'translateX',
+    },
+    duration: 1000,
+    delay: 0,
+    easing: 'linear',
+    autoplay: false,
   }
 
   class AnimeFacade {
@@ -19,17 +37,45 @@
         throw new Error('No target selector');
       }
 
-      if(!options.preset) {
-        throw new Error('Choose preset for animation');
+      this.targets = typeof(targets) === 'string' ? document.querySelectorAll(targets) : targets;
+      this.options = options || {};
+
+      if(!Object.keys(this.options).length) {
+        return
+      }
+      else {
+        this.options.preset.name = options.preset.name || defaults.preset.name;
+        this.options.duration = options.preset.duration || defaults.duration;
+        this.options.delay = options.preset.delay || defaults.delay;
+        this.options.easing = options.preset.easing || defaults.easing;
+        this.options.autoplay = options.preset.autoplay || defaults.autoplay;
       }
 
-      this.targets = targets;
-      this.options = options || {};
       this.getChosenPreset(presets);
-      this.initOnScroll();
+      this.initOptions();
+      this.initOnLoad();
+      this.startOnScroll();
     }
 
-    initOnScroll() {
+    initOptions() {
+
+      let that = this;
+
+      that.targets.forEach(function(targetElement) {
+          that.getOptions(targetElement, that.getChosenPreset(presets));
+      })
+    }
+
+    initOnLoad() {
+
+      let that = this;
+
+        that.targets.forEach(function(targetElement) {
+          that.initAnime(that.getOptions(targetElement, that.getChosenPreset(presets)));
+        })
+    }
+
+    startOnScroll() {
 
       let that = this;
 
@@ -37,8 +83,9 @@
         that.targets.forEach(function(targetElement) {
           let windowHeight = window.innerHeight;
           let targetPosition = targetElement.getBoundingClientRect().top;
-          if (targetPosition - windowHeight <= 0) {
-            that.initAnime(targetElement, that.getChosenPreset(presets));
+          if (targetPosition - windowHeight <= 0 && !targetElement.classList.contains('animated')) {
+            that.initAnime(that.getOptions(targetElement, that.getChosenPreset(presets))).play();
+            targetElement.classList.add('animated');
           }
         })
       })
@@ -52,21 +99,35 @@
 
       for(let preset in presets) {
         if(presets.hasOwnProperty(preset)) {
-          if(that.options.preset === preset) {
-            matchedObject = JSON.stringify({ [preset]: presets[preset] });
+          if(that.options.preset.name === preset) {
+            matchedObject = JSON.stringify({ [preset]: that.options.preset.params || presets[preset] });
           }
         }
       }
       return matchedObject;
     }
 
-    initAnime(targetElement, preset) {
+    getOptions(targetElement, preset) {
+
+      let that = this;
 
       let data = Object.assign({
-        targets: targetElement
+        targets: targetElement,
+        duration: that.options.duration,
+        delay: that.options.delay,
+        easing: that.options.easing,
+        autoplay: that.options.autoplay
       }, JSON.parse(preset));
 
+      return data;
+    }
+
+    initAnime(data) {
       anime(data);
+
+      let animeInstance = anime(data);
+
+      return animeInstance;
     }
   }
 

7c468eddc2b6b549f855f14970188cd8f1db10d6
dmitriy-akkerman <dmitriy.akkerman.odesseo@gmail.com>
2020-09-08 11:45:51 +0300

    коммит для новой ветки

--- src/js/presets.js
+++ src/js/presets.js
@@ -1,40 +1,5 @@
 export default {
-  fadeIn: {
-    opacity: [0, 1]
+  headerAnimation1: {
+
   },
-  fadeInDown: {
-    opacity: [0, 1],
-    translateY: ["-100%", "0"]
-  },
-  fadeInUp: {
-    opacity: [0, 1],
-    translateY: ["100%", "0"]
-  },
-  fadeInLeft: {
-    opacity: [0, 1],
-    translateX: ["-100%", "0"]
-  },
-  fadeInRight: {
-    opacity: [0, 1],
-    translateX: ["100%", "0"]
-  },
-  fadeOut: {
-    opacity: [1, 0]
-  },
-  fadeOutDown: {
-    opacity: [1, 0],
-    translateY: ["0", "100%"]
-  },
-  fadeOutUp: {
-    opacity: [1, 0],
-    translateY: ["0", "100%"]
-  },
-  fadeOutLeft: {
-    opacity: [1, 0],
-    translateX: ["0", "100%"]
-  },
-  fadeOutRight: {
-    opacity: [1, 0],
-    translateX: ["0", "100%"]
-  }
 }

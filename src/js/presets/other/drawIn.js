cd129417a61ee834293695e584091a5d22102e1a
dmitriy-akkerman <dmitriy.akkerman.odesseo@gmail.com>
2020-09-18 10:44:23 +0300

    функция прорисовки пути svg

--- src/js/presets/other/drawIn.js
+++ src/js/presets/other/drawIn.js
@@ -1,7 +1,11 @@
 //Прорисовка обводки svg элемента
 
 module.exports = {
-    strokeDashoffset: [anime.setDashoffset, 0],
+    strokeDashoffset: function () {
+        if(anime) {
+            return [anime.setDashoffset, 0]
+        }
+    },
     easing: 'easeInOutSine',
     duration: 1500,
     delay: function(el, i) {

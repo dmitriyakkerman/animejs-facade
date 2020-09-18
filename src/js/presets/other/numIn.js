2b8f16583a1dbb18a214c292f11a59f282acddc5
dmitriy-akkerman <dmitriy.akkerman.odesseo@gmail.com>
2020-09-18 10:57:22 +0300

    проработал функцию анимации чисел

--- src/js/presets/other/numIn.js
+++ src/js/presets/other/numIn.js
@@ -1,7 +1,12 @@
 //Анимация числового значения элемента от и до
 
 module.exports = {
-    innerHTML: [0, 100],
+    innerHTML: function(el) {
+        return [0, el.dataset.n]
+    },
+    delay: function(el, i) {
+        return i * 100
+    },
     duration: 1000,
     round: 1
 }

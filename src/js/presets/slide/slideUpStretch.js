485f9d0e86fe1760463541767eeee2cbdc936808
dmitriyakkerman <darksider22023103>
2020-09-25 10:20:34 +0300

    пресет slideUpStretch

---
+++ src/js/presets/slide/slideUpStretch.js
@@ -0,0 +1,12 @@
+//Выдвижение элемента из родительского блока: родительский блок меняет высоту и выдвигается снизу вверх
+
+const anime = require('../../anime.min');
+
+module.exports = {
+    height: function(el) {
+      return [0, window.getComputedStyle(el).height]
+    },
+    translateY: ['200px', 0],
+    duration: 1500,
+    easing: "easeInQuint"
+}
